7d88ed13e2cbbd48a2d723326f3dac6a8913987a
karthikkadimi <96563776+karthikkadimi@users.noreply.github.com>
2021-12-24 10:56:13 +0530

    Add files via upload

---
+++ condition.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{ 
+int a;
+printf( "enter the value of a");
+scanf("%d",&a);
+if(a<0)
+{printf("the number is negative");
+
+}
+else
+{
+	printf( "the number is positive");
+}
+}
