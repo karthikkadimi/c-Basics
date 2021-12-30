7cb91b053e2294953c5e1353cd98c37c224598e1
karthikkadimi <96563776+karthikkadimi@users.noreply.github.com>
2021-12-30 10:49:49 +0530

    Add files via upload

---
+++ divisible num.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+main()
+{
+
+ int a;
+ printf("enter the value of a");
+ scanf("%d,&a");
+ if(a%1==0)
+ {
+ 	printf("divisible by 1\n");
+ }
+ if(a%2==0)
+ {
+ 	printf("divisible by 2\n");
+ }
+ if( a%3==0)
+ {
+ 	printf("divisible by 3\n");
+ }
+ if(a%4==0)
+ {
+ 	printf("divisible by 4\n");
+ }
+ if(a%5==0)
+ {
+ 	printf("divisible by 5\n");
+ }
+ if(a%6==0)
+ {
+ 	printf("divisible by 6\n");
+ }
+ if(a%7==0)
+ {
+ 	printf("divisible by 7\n");
+ }
+ if(a%8==0)
+ {
+ 	printf("divisible by 8\n");
+ }
+ if(a%9==0)
+ {
+ 	printf("divisible by 9\n");
+ }
+ else
+ {
+ 	printf("not divisible by any number");
+ }
+}
