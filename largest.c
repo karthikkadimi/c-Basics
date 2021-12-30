e907fe8febe780694a2a251ac47ef6cb2db71583
karthikkadimi <96563776+karthikkadimi@users.noreply.github.com>
2021-12-30 10:50:14 +0530

    Add files via upload

---
+++ largest.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+main()
+{
+	int a,b,c,d;
+	printf("enter the values of a,b,c,d");
+	scanf("%d %d %d %d",&a,&b,&c,&d);
+	if(a>b&&a>c&&a>d)
+	{
+		printf("%d",a);
+	}
+	else if(b>a&&b>c&&b>d)
+	{
+		printf("%d",b );
+	}
+	else if(c>a&&c>b&&c>d)
+	{
+		printf("%d",c );
+	}
+	else
+	{
+		printf("%d",d);
+	}
+}
+
