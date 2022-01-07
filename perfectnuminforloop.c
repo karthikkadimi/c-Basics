dbe02f29d81c93f2d4da7c8d7df5c2b7a4bdb24c
karthikkadimi <96563776+karthikkadimi@users.noreply.github.com>
2022-01-06 23:37:06 -0600

    Add files via upload

---
+++ perfectnuminforloop.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+main()
+{
+	int i,n,sum=0;
+	scanf("%d",&n);
+	for(i=1;i<n;i++)//
+	{
+	if(n%i==0)
+	{
+	sum += i;
+    }
+	}
+	if (sum==n)
+	{
+		printf("perfect");
+	}
+	else
+	{
+		printf("not perfect");
+	}
+	
+}
